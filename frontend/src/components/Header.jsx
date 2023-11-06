ae77ebf3ba50087a15d35158240c9397de197ed9
Uneku Ogbaje <unekuogbaje@gmail.com>
2023-11-06 15:25:18 +0100

    Implemented ratings

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Nav, Navbar, Container } from 'react-bootstrap';
 import { FaUser, FaShoppingCart } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap'
 import logo from '../assets/logo.svg'
 
 const Header = () => {
@@ -7,14 +8,26 @@ const Header = () => {
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>
-            <img src={logo} alt='RetailJoy'/>
-          </Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>
+              <img src={logo} alt='BuyJoy'/>
+            </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav'/>
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <Nav.Link href='/cart'><FaShoppingCart/>Cart</Nav.Link>
-              <Nav.Link href='/login'><FaUser/>Sign-in</Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <FaShoppingCart/>
+                  Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <FaUser/>
+                  Sign-in
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
