27eee615059aef1ac30885085e4fe152735d3e2f
Uneku Ogbaje <unekuogbaje@gmail.com>
2023-11-11 20:38:43 +0100

    product description

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <Container>
         <Row>
           <Col className='text-center py-3 '>
-            <p>BuyPro &copy; {currentYear}</p>
+            <p>BuyJoy &copy; {currentYear}</p>
           </Col>
         </Row>
       </Container>
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -35,13 +35,55 @@ const ProductScreen = () => {
             </ListGroup.Item>
 
             <ListGroup.Item>
-              <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+              <Rating 
+              value={product.rating} 
+              text={`${product.numReviews} reviews`}
+              />
             </ListGroup.Item>
+
+            <ListGroup.Item>
+              Price: ${product.price}
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              Description: {product.description}
+            </ListGroup.Item>
+            
           </ListGroup>
         </Col>
 
         <Col md={3}>
-        
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Button
+                className='btn-block'
+                type='button'
+                disabled={product.countInStock === 0}
+                >
+                  Add to Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
         </Col>
       </Row>
     </Fragment>
